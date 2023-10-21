df97fb16576ea137c048446489cf30736b99904a
Roniselton <ronibarreto@gmail.com>
2023-10-21 00:40:54 -0300

    Contador

---
+++ src/br/dio/roniselton/Contador.java
@@ -0,0 +1,35 @@
+package br.dio.roniselton;
+
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        }catch ( ParametroInvalidoException exception) {
+            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.err.println("O Primeiro parametro deve ser maior que o Primeiro!!!");
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametroInvalidoException {
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if(parametroUm > parametroDois)
+            throw new ParametroInvalidoException();
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 0; i < contagem ; i++) {
+            System.out.println("Imprimindo Numero " + (i +1) );
+
+        }
+    }
+}
---
+++ src/br/dio/roniselton/ParametroInvalidoException.java
@@ -0,0 +1,4 @@
+package br.dio.roniselton;
+
+public class ParametroInvalidoException extends Exception {
+}
